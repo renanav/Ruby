1ce02720e3315ed13335ef08fa772093e3634b22
renanav <renan.avrahami@gmail.com>
2016-11-12 20:42:18 -0500

    Add separartor, clean code

--- shopping_list.rb
+++ shopping_list.rb
@@ -12,15 +12,36 @@ def add_list_item
   print "How many? "
   quantity = gets.chomp.to_i
 
-  hash = { "name" => item_name, "quantity" => quantity}
+  hash = { "name" => item_name, "quantity" => quantity }
   return hash
 end
 
+#for a separator
+def print_separator(character="-")
+  puts character * 80
+end
+
+#a method to print the list
+def print_list(list)
+  puts "List: #{list['name']}"
+  print_separator
+
+  list["items"].each do |item|
+    puts "\tItem: " + item['name'] + "\t\t\t" +
+         "quantity: " + item['quantity'].to_s
+  end
+
+  print_separator
+end
+
 list = create_list()
-puts list.inspect
+puts "Lets add some items to your list"
 
 #push the items hash into the items Array
 list['items'].push(add_list_item())
+list['items'].push(add_list_item())
+list['items'].push(add_list_item())
 
 #the final list
-puts list.inspect
+puts "Here is you list"
+print_list(list)
