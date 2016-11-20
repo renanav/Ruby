23e2800bca9ef4bd6ee2ae07788779a34bcf4e0d
renanav <renan.avrahami@gmail.com>
2016-11-20 17:00:25 -0500

    Create add_contact method and a while loop to add more contacts

--- contact_list.rb
+++ contact_list.rb
@@ -1,6 +1,3 @@
-# a contact list array
-contact_list = []
-
 # a method that I'll call every time I'm asking a question
 def ask(question, kind="string")
   print question + " "
@@ -10,5 +7,28 @@ def ask(question, kind="string")
   return answer
 end
 
-answer = ask("What is your name?")
-puts answer
+# a method to add contacts to the list. a contact is a hash, since one contact can have more than one number, the phone number value is an array
+def add_contact
+    contact = {"name" => "", "phone_numbers" => []}
+    contact["name"] = ask("What is the person's name?")
+    # a loop to add a phone number to a contact
+    answer = ""
+    while answer != "n"
+      answer = ask("Would you like to add a phone number? (y/n)")
+      if answer == "y"
+        phone = ask("Enter a phone number:")
+        contact["phone_numbers"].push(phone)
+      end
+    end
+    return contact
+end
+
+# a contact list array
+contact_list = []
+
+answer = ""
+while answer != "n"
+  #I'm adding the result of the add_contact method to the contact_list array
+  contact_list.push(add_contact())
+  answer = ask("Add another? (y/n)")
+end
